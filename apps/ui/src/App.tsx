11c67c2925965add94323733a121d5c8f12ab61e
Nicolás Ludueña <luduena.nicolas.victorio@gmail.com>
2025-08-24 00:22:58 -0300

    feat: website skeleton with react + vite + tailwind + shadcn set up

---
+++ apps/ui/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <>
+      <Button>Hola</Button>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
